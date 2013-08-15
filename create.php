828af2330c5630bae5d3faaed286aac6d2a2e0c7
fifth <523234748@qq.com>
2013-08-15 14:28:44 +0800

    2013.8.15

--- create.php
+++ create.php
@@ -19,40 +19,72 @@
 	$objPHPExcel->getActiveSheet()->setTitle('uyan comments');
 
 	// 设置单元格的值
-	$objPHPExcel->getActiveSheet()->setCellValue('A1', '时间');
-	$objPHPExcel->getActiveSheet()->setCellValue('B1', '发言人');
-	$objPHPExcel->getActiveSheet()->setCellValue('C1', '来自');
-	$objPHPExcel->getActiveSheet()->setCellValue('D1', '内容');
-	$objPHPExcel->getActiveSheet()->mergeCells('A1:A2');
-	$objPHPExcel->getActiveSheet()->mergeCells('B1:B2');
-	$objPHPExcel->getActiveSheet()->mergeCells('C1:C2');
-	$objPHPExcel->getActiveSheet()->mergeCells('D1:D2');
+	$objPHPExcel->getActiveSheet()->setCellValue('A1', '序号');
+	$objPHPExcel->getActiveSheet()->setCellValue('B1', '时间');
+	$objPHPExcel->getActiveSheet()->setCellValue('C1', '发言人');
+	$objPHPExcel->getActiveSheet()->setCellValue('D1', '来自');
+	$objPHPExcel->getActiveSheet()->setCellValue('E1', '内容');
+	// $objPHPExcel->getActiveSheet()->mergeCells('A1:A2');
+	// $objPHPExcel->getActiveSheet()->mergeCells('B1:B2');
+	// $objPHPExcel->getActiveSheet()->mergeCells('C1:C2');
+	// $objPHPExcel->getActiveSheet()->mergeCells('D1:D2');
 
-	$objPHPExcel->getActiveSheet()->setCellValue('E1', '回复');
-	$objPHPExcel->getActiveSheet()->mergeCells('E1:H1');
-	$objPHPExcel->getActiveSheet()->setCellValue('E2', '时间');
-	$objPHPExcel->getActiveSheet()->setCellValue('F2', '发言人');
-	$objPHPExcel->getActiveSheet()->setCellValue('G2', '来自');
-	$objPHPExcel->getActiveSheet()->setCellValue('H2', '内容');
+	$objPHPExcel->getActiveSheet()->setCellValue('F1', '回复');
+	$objPHPExcel->getActiveSheet()->setCellValue('G1', '类别');
+	$objPHPExcel->getActiveSheet()->setCellValue('H1', '来自求是潮');
+	// $objPHPExcel->getActiveSheet()->mergeCells('E1:H1');
+	// $objPHPExcel->getActiveSheet()->setCellValue('E2', '时间');
+	// $objPHPExcel->getActiveSheet()->setCellValue('F2', '发言人');
+	// $objPHPExcel->getActiveSheet()->setCellValue('G2', '来自');
+	// $objPHPExcel->getActiveSheet()->setCellValue('H2', '内容');
 	
-	$i=3;
+	$i=1;
+	$index=0;
 	foreach ($content as $key => $value) {
-		$objPHPExcel->getActiveSheet()->setCellValue('A'.$i,$value['time']);
-		$objPHPExcel->getActiveSheet()->setCellValue('B'.$i,$value['uname']);
-		$objPHPExcel->getActiveSheet()->setCellValue('C'.$i,$value['ulink']);
-		$objPHPExcel->getActiveSheet()->setCellValue('D'.$i,$value['content']);
+		$index++;		
 		$i++;
+		$objPHPExcel->getActiveSheet()->setCellValue('A'.$i,$index);
+		$objPHPExcel->getActiveSheet()->setCellValue('B'.$i,$value['time']);
+		$objPHPExcel->getActiveSheet()->setCellValue('C'.$i,$value['uname']);
+		// $objPHPExcel->getActiveSheet()->setCellValue('D'.$i,$value['ulink']);
+		if (!$value['ulink']) {
+			$objPHPExcel->getActiveSheet()->setCellValue('D'.$i,'游客');
+		} elseif (stripos($value['ulink'], 'qq')) {
+			$objPHPExcel->getActiveSheet()->setCellValue('D'.$i,'空间');
+		} elseif (stripos($value['ulink'], 'weibo')) {
+			$objPHPExcel->getActiveSheet()->setCellValue('D'.$i,'微博');
+		} elseif (stripos($value['ulink'], 'renren')) {
+			$objPHPExcel->getActiveSheet()->setCellValue('D'.$i,'人人');
+		} else {
+			$objPHPExcel->getActiveSheet()->setCellValue('D'.$i,'其它');
+		}
+		$objPHPExcel->getActiveSheet()->setCellValue('E'.$i,$value['content']);
+		$objPHPExcel->getActiveSheet()->setCellValue('F'.$i,0);
+		$fatherindex=$index;
 		if (isset($value['child'])) {
 			foreach ($value['child'] as $k => $v) {
-				$objPHPExcel->getActiveSheet()->setCellValue('E'.$i,$v['time']);
-				$objPHPExcel->getActiveSheet()->setCellValue('F'.$i,$v['uname']);
-				$objPHPExcel->getActiveSheet()->setCellValue('G'.$i,$v['ulink']);
-				$objPHPExcel->getActiveSheet()->setCellValue('H'.$i,$v['content']);
+				$index++;
 				$i++;
+				$objPHPExcel->getActiveSheet()->setCellValue('A'.$i,$index);
+				$objPHPExcel->getActiveSheet()->setCellValue('B'.$i,$v['time']);
+				$objPHPExcel->getActiveSheet()->setCellValue('C'.$i,$v['uname']);
+				if (!$v['ulink']) {
+					$objPHPExcel->getActiveSheet()->setCellValue('D'.$i,'游客');
+				} elseif (stripos($v['ulink'], 'qq')) {
+					$objPHPExcel->getActiveSheet()->setCellValue('D'.$i,'空间');
+				} elseif (stripos($v['ulink'], 'weibo')) {
+					$objPHPExcel->getActiveSheet()->setCellValue('D'.$i,'微博');
+				} elseif (stripos($v['ulink'], 'renren')) {
+					$objPHPExcel->getActiveSheet()->setCellValue('D'.$i,'人人');
+				} else {
+					$objPHPExcel->getActiveSheet()->setCellValue('D'.$i,'其它');
+				}
+				$objPHPExcel->getActiveSheet()->setCellValue('E'.$i,$v['content']);
+				$objPHPExcel->getActiveSheet()->setCellValue('F'.$i,$fatherindex);
 			}
 		}
 	}
-	echo $i-2;
+	echo $i-1;
 
 	// 保存excel—2007格式
 	$objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
